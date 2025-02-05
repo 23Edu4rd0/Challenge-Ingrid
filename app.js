7eaaedb90abdbfa9b5bec454b5c513757d5e2852
Ingrid Heloise <ingridheloisesantos@gmail.com>
2025-02-05 15:05:49 -0300

    Final do Projeto

--- app.js
+++ app.js
@@ -1,49 +1,69 @@
 let amigos = [];
-let lista = [];
 
 function validarNome() {
-    let nome = document.querySelector('input').value;
-    if (nome == '') {
+    let nome = document.querySelector('#nomeAdicionado').value;
+    if (amigos.includes(nome)){
+        alert(`O nome ${nome} já foi adicionado`);
+    }
+    else if(nome == '') {
         alert("Por favor, digite um nome válido!!");
     }
     else {
-        console.log(`O nome ${nome} é válido`);
         amigos.push(nome);
+        limparListas();
         aparecerNomeAmigo();
-        limparCampo();
+        limparCampo('nomeAdicionado');
+    }
+}
+
+function removerNome() {
+    let nomeAmigo = document.querySelector('#nomeRemovido').value;
+    if (amigos.includes(nomeAmigo)){
+        amigos = amigos.filter(amigo => amigo != nomeAmigo);
+        alert(`O amigo ${nomeAmigo} foi excluído com sucesso!`);
+        limparCampo('nomeRemovido');
+        limparListas();
+        aparecerNomeAmigo();
+    }
+    else if (nomeAmigo == '') {
+        alert("Nenhum nome selecionado");
+        limparCampo('nomeRemovido');
+    }
+    else {
+        alert(`O nome ${nomeAmigo} não foi adicionado!`);
     }
 }
 
 function aparecerNomeAmigo() {
     let listaAmigos = document.querySelector('#listaAmigos');
     for (let i = 0; i < amigos.length; i++) {
-        lista[i] = `<li>${amigos[i]}</li>`;
-        listaAmigos.innerHTML = lista;
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
     }
 }
 
-function limparCampo() {
-    nome = document.querySelector('input');
+function limparListas(){
+    document.querySelector('#listaAmigos').innerHTML = "";
+    document.querySelector('#resultado').innerHTML = "";
+}
+
+function limparCampo(campo) {
+    nome = document.getElementById(campo);
     nome.value = '';
-    console.log(amigos);
 }
 
 function sortearAmigo() {
     if (amigos.length > 0) {
-        let index = parseInt(Math.random() * amigos.length + 1);
+        let index = parseInt(Math.random() * amigos.length);
         let amigoSecreto = amigos[index];
         let resultado = document.querySelector('#resultado');
         resultado.innerHTML = `<li>O amigo secreto sorteado é: ${amigoSecreto}</li>`;
-        reiniciar();
     }
     else {
         validarNome();
     }
 }
 
-function reiniciar() {
-    console.log("Reiniciando...")
-    lista = [];
+function novoSorteio() {
+    limparListas();
     amigos = [];
-    aparecerNomeAmigo();
 }
